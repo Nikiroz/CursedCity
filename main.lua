2927af706c891f0690c0b8e482d6ae01b9965286
Nikita <nikirozzz@gmail.com>
2020-01-20 20:12:18 +0300

    init

---
+++ main.lua
@@ -0,0 +1,14 @@
+-- $Name: [URQ] Cursed City$
+-- $Name(ru): [URQ] Проклятый Город
+-- $Version: 1.6.1$
+-- $Author: Беляев Никита aka Nikitoz$
+-- $E-mail: Nikitozbel@mail.ru$
+-- $Adaptation: v.v.b.$
+-------------
+-- fixed root dir > urq-cursed_city by j-maks Nov-17, 2013
+-- add 'urq' dir  dec 02, 2013  j-maks (jmaksima_at_gmail_dot_com)
+-------------
+URQ_PATH = './urq/'
+dofile "./urq/urq.lua"
+-------------
+lurq 'cursed_city.qst'
